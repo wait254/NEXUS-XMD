5ed0501723e4a6f0ba5d947d52cb5f3b7ece140f
🐐 Mr pkdriller 🐐 <benardwachira49@gmail.com>
2025-05-21 04:28:23 +0300

    Update auto-reply.js

--- plugins/auto-reply.js
+++ plugins/auto-reply.js
@@ -6,7 +6,7 @@ const {cmd , commands} = require('../command')
 
 
 // Replace this with your actual GitHub RAW JSON URL
-const GITHUB_RAW_URL = 'https://raw.githubusercontent.com/popkidxtech/POP-DATA/main/autoreply.json';
+const GITHUB_RAW_URL = 'https://raw.githubusercontent.com/NEXUS-XMD/Pkdriller-DATA/main/autoreply.json';
 
 cmd({
   on: "body"
